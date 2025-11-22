6a1c67c3265d0304b61d1bef5118d1530a618b99
Jesus Zabdiel Hernandez Martinez <2023371177@uteq.edu.mx>
2025-11-22 15:34:42 -0600

    listo

--- src/main/java/com/theworkers/filesmicroservice/FilesMicroserviceApplication.java
+++ src/main/java/com/theworkers/filesmicroservice/FilesMicroserviceApplication.java
@@ -2,8 +2,18 @@ package com.theworkers.filesmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+// 🎯 CRÍTICO: Agregar la exclusión de Actuator Security
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+        UserDetailsServiceAutoConfiguration.class,
+        // 💡 NUEVA EXCLUSIÓN: Para resolver el error de Actuator
+        ManagementWebSecurityAutoConfiguration.class
+})
 public class FilesMicroserviceApplication {
 
     public static void main(String[] args) {
